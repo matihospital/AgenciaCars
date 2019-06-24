8ef4ea285da3c9c5cd584c122d91e46da4f3668a
enzoRossi <enzofrossi95@gmail.com>
2019-06-24 20:03:39 -0300

    no message

--- AgenciaCars/clases/acceso_BD.cs
+++ AgenciaCars/clases/acceso_BD.cs
@@ -16,7 +16,7 @@ namespace AgenciaCars.clases
 
         OleDbConnection conexion = new OleDbConnection();
         OleDbCommand cmd = new OleDbCommand();
-        string cadena_conexion = "Provider=SQLNCLI11;Data Source=MATI\\SQLEXPRESS;Integrated Security=SSPI;Initial Catalog=AgenciaCars;";
+        string cadena_conexion = @"Provider=SQLNCLI11;Data Source=Rossi-Pc\sqlenzo;Integrated Security=SSPI;Initial Catalog=AgenciaCars";
         //string cadena_conexion = "Provider=SQLNCLI11;Data Source=NBDELLPABLO;Integrated Security=SSPI;Initial Catalog=AgenciaCars";
         //string cadena_conexion = @"Provider=SQLNCLI11;Data Source=Rossi-Pc\sqlenzo;Integrated Security=SSPI;Initial Catalog=AgenciaCars";
         //string cadena_conexion = @"Provider=SQLNCLI11;Data Source=Rossi-Pc\sqlenzo;Integrated Security=SSPI;Initial Catalog=AgenciaCars";
